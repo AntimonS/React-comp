9baeb67018c6850c0063935eeac80afd7bbf7cf2
Ante Debelić <antedebelic@yahoo.com>
2023-11-07 14:18:02 +0100

    added sortabletable.js

---
+++ src/components/SortableTable.js
@@ -0,0 +1,27 @@
+import Table from "./Table";
+
+function SortableTable(props) {
+  const { config } = props;
+
+  const handleClick = (label) => {
+    console.log(label);
+  };
+
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+    return {
+      ...column,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {column.lable} IS SORTABLE
+        </th>
+      ),
+    };
+  });
+
+  return <Table {...props} config={updatedConfig} />;
+}
+
+export default SortableTable;
